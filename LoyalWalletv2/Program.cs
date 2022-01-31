398985ed50ec454df94568bcbf431772fb653cb9
constadry <behappydtworry@gmail.com>
2022-01-31 13:21:18 +0300

    feat: added auth di and conn string

--- LoyalWalletv2/Program.cs
+++ LoyalWalletv2/Program.cs
@@ -1,8 +1,11 @@
+using System.Text;
 using LoyalWalletv2;
 using LoyalWalletv2.Controllers;
+using LoyalWalletv2.Domain.Models.AuthenticationModels;
 using Newtonsoft.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,6 +31,28 @@ builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddIdentity<User, IdentityRole>()  
+    .AddEntityFrameworkStores<AppDbContext>()  
+    .AddDefaultTokenProviders();
+builder.Services.AddAuthentication(options =>  
+    {  
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;  
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;  
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;  
+    })
+    .AddJwtBearer(options =>  
+    {  
+        options.SaveToken = true;  
+        options.RequireHttpsMetadata = false;  
+        options.TokenValidationParameters = new TokenValidationParameters()  
+        {  
+            ValidateIssuer = true,  
+            ValidateAudience = true,  
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],  
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],  
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))  
+        };  
+    });  
 
 var app = builder.Build();
 
