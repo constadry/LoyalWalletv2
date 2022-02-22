9b9172734ff823beaea2a30edcfe20ef90894556
constadry <behappydtworry@gmail.com>
2022-02-22 13:20:55 +0300

    feat: new tests

--- LoyalWalletV2.Tests/DomainTests.cs
+++ LoyalWalletV2.Tests/DomainTests.cs
@@ -5,12 +5,12 @@ namespace LoyalWalletV2.Tests;
 
 public class DomainTests
 {
-    private Company _company;
-    private Customer _customer;
+    private readonly Customer _customer;
+    private readonly Employee _employee;
 
     public DomainTests()
     {
-        _company = new Company
+        var company = new Company
         {
             MaxCountOfStamps = 6,
             Id = 1
@@ -19,8 +19,13 @@ public class DomainTests
         _customer = new Customer
         {
             PhoneNumber = "+7 951 8270 540",
-            CompanyId = 1,
-            Company = _company
+            CompanyId = company.Id,
+            Company = company
+        };
+
+        _employee = new Employee
+        {
+            CompanyId = company.Id,
         };
     }
 
@@ -29,21 +34,24 @@ public class DomainTests
     {
         const uint expectedStamps = 1;
 
-        _customer.AddStamp();
+        _customer.AddStamp(_employee);
 
         Assert.Equal(expectedStamps, _customer.CountOfStamps);
+        Assert.Equal(expectedStamps, _employee.CountOfStamps);
     }
 
     [Fact]
     public void AddSixStamps_AndTakeOnePresent()
     {
         const uint expectedPresents = 0;
+        const uint expectedGivenPresents = 1;
 
         var i = 0;
-        for (; i < 6; i++) _customer.AddStamp();
+        for (; i < 6; i++) _customer.AddStamp(_employee);
         
-        _customer.TakePresent();
+        _customer.TakePresent(_employee);
 
         Assert.Equal(expectedPresents, _customer.CountOfStoredPresents);
+        Assert.Equal(expectedGivenPresents, _employee.CountOfPresents);
     }
 }
