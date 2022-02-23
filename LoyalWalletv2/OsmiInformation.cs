dd33335e5dd21c3ffbbba9a56369126c0dc136a3
constadry <behappydtworry@gmail.com>
2022-02-23 16:39:03 +0300

    feat: added info

--- LoyalWalletv2/OsmiInformation.cs
+++ LoyalWalletv2/OsmiInformation.cs
@@ -2,8 +2,8 @@ namespace LoyalWalletv2;
 
 public static class OsmiInformation
 {
-    public static string? HostPrefix { get; set; } = "https://api.osmicards.com";
-    
-    public static string? ApiId { get; set; }
-    public static string? ApiKey { get; set; }
+    public static string HostPrefix => "https://api-dev.osmicards.com/v2t";
+
+    public static string ApiId => "5GRO30Z1VWVTCW9ULL1D";
+    public static string ApiKey => "edc03665bb4043b1950448d3807b2947e4a3218c";
 }
