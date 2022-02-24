789e172aee6e66af08a495436aa206fa83c89e0e
constadry <behappydtworry@gmail.com>
2022-02-24 16:33:36 +0300

    tests: added integration test od count stamps

--- LoyalWalletV2.Tests/ApiTests.cs
+++ LoyalWalletV2.Tests/ApiTests.cs
@@ -1,7 +1,10 @@
-using System;
-using System.Collections.Generic;
-using LoyalWalletv2;
-using LoyalWalletv2.Contexts;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using LoyalWalletv2.Domain.Models.AuthenticationModels;
+using LoyalWalletV2.Tests.Extensions;
+using Newtonsoft.Json;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -11,26 +14,56 @@ public class ApiTests
 {
     private readonly ITestOutputHelper _testOutputHelper;
 
-    public ApiTests(ITestOutputHelper testOutputHelper, AppDbContext context)
+    public ApiTests(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
     }
 
     [Fact]
-    public async void AddNewCustomer()
+    public async void AddLocation_AddEmployee()
     {
-        // var values = new Dictionary<string, string>
-        // {
-        //     { "phoneNumber", "+7 (951) 8270 540" },
-        //     { "companyId", "1" }
-        // };
-        //
-        // var content = new FormUrlEncodedContent(values);
-        //
-        // using var requestMessage =
-        //     new HttpRequestMessage(HttpMethod.Post, "https://localhost:7005/api/Osmi/clients/add");
-        // requestMessage.Content = content;
-        // var responseMessage = await _httpClient.SendAsync(requestMessage);
-        // _testOutputHelper.WriteLine(requestMessage.Content.ToString());
+        await using var application = new CustomWebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+
+        var loginRequest = new LoginModel
+        {
+            Email = "kostya.adrianov@gmail.com",
+            Password = "Password123#"
+        };
+
+        var serializedValues = JsonConvert.SerializeObject(loginRequest);
+        
+        using var requestMessage =
+            new HttpRequestMessage(HttpMethod.Post, "api/Authenticate/login");
+
+        requestMessage.Content = new StringContent(
+            serializedValues,
+            Encoding.UTF8,
+            "application/json");
+        
+        var response = await client.SendAsync(requestMessage);
+        
+        var responseSerialised = await response.Content.ReadAsStringAsync();
+
+        var loginResponse = JsonConvert.DeserializeObject<LoginResponse>(responseSerialised);
+        _testOutputHelper.WriteLine(responseSerialised);
+    
+        using var requestMessage2 =
+            new HttpRequestMessage(HttpMethod.Get, "/api/Customer/1/all-stamps-count");
+        
+        requestMessage2.Headers.Authorization =
+            new AuthenticationHeaderValue("Bearer", loginResponse.Token);
+        
+        response = await client.SendAsync(requestMessage2);
+        
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal("1", await response.Content.ReadAsStringAsync());
     }
+}
+
+public class LoginResponse
+{
+    public string? Token { get; set; }
+    public string? Expiration { get; set; }
+    public int CompanyId { get; set; }
 }
