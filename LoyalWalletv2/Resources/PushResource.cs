9d2d45d6cd1569b61a7c0732cd6901e79204a1c5
constadry <behappydtworry@gmail.com>
2022-03-01 14:46:37 +0300

    fix: company's not used

--- LoyalWalletv2/Resources/PushResource.cs
+++ LoyalWalletv2/Resources/PushResource.cs
@@ -3,6 +3,5 @@ namespace LoyalWalletv2.Resources;
 public class PushResource
 {
     public string? Message { get; set; }
-    public int CompanyId { get; set; }
     public List<string>? SerialNumbers { get; set; }
 }
